2ecb5da7b8d35622cf8c157b620b277c872ac1ef
raaj <rajesh.y@everest.engineering>
2021-10-05 13:28:53 +0530

    tested for the correct score in the probabilityOfPlayers function

--- src/tests/app.test.ts
+++ src/tests/app.test.ts
@@ -7,8 +7,13 @@ test("gives the probability of platers as mentioned", () => {
   });
   test("failing at a specific index in the array of probability", () => {
     const kirat=probabilityOfPlayers(5,30,25,10,15,1,9,5);
-    const score=kirat[5]
+    const score=kirat[4]
      expect(score).toBe(0);
+   });
+   test("failing at a specific index in the array of probability", () => {
+    const kirat=probabilityOfPlayers(5,30,25,10,15,1,9,5);
+    const score=kirat[5]
+     expect(score).toBe(1);
    });
 
 
