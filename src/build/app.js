94e048965c3d1a8f908540d54e6e2d03680a8f83
raaj <rajesh.y@everest.engineering>
2021-10-05 20:13:14 +0530

    solved the error 'export module not found' in the app.js file,when it used in the HTML file.

--- src/build/app.js
+++ src/build/app.js
@@ -2,22 +2,22 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.probabilityOfPlayers = void 0;
 function probabilityOfPlayers(dot, one, two, three, four, five, six, out) {
-    var arrayOfProbabilities = [];
-    for (var ball = 0; ball < dot; ball++)
+    let arrayOfProbabilities = [];
+    for (let ball = 0; ball < dot; ball++)
         arrayOfProbabilities.push(0);
-    for (var ball = 0; ball < one; ball++)
+    for (let ball = 0; ball < one; ball++)
         arrayOfProbabilities.push(1);
-    for (var ball = 0; ball < two; ball++)
+    for (let ball = 0; ball < two; ball++)
         arrayOfProbabilities.push(2);
-    for (var ball = 0; ball < three; ball++)
+    for (let ball = 0; ball < three; ball++)
         arrayOfProbabilities.push(3);
-    for (var ball = 0; ball < four; ball++)
+    for (let ball = 0; ball < four; ball++)
         arrayOfProbabilities.push(4);
-    for (var ball = 0; ball < five; ball++)
+    for (let ball = 0; ball < five; ball++)
         arrayOfProbabilities.push(5);
-    for (var ball = 0; ball < six; ball++)
+    for (let ball = 0; ball < six; ball++)
         arrayOfProbabilities.push(6);
-    for (var ball = 0; ball < out; ball++)
+    for (let ball = 0; ball < out; ball++)
         arrayOfProbabilities.push(7);
     return arrayOfProbabilities;
 }
@@ -29,8 +29,12 @@ according to the given probability
 for example: if we give the probabilities of a player as
 const kirat=probabilityOfPlayers(5,30,25,10,15,1,9,5);
 then it return an array as
-[0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2,
-      2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3,
-       3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-        5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7]   */
+[0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+ 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4,
+ 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7]
+    according to the definition of probaility in the above array
+    there is 5% of probability to get "0" ,if we pick at random
+    there is 30% of probability to get "1", if we pick at random and so on..*/
+    const kirat=probabilityOfPlayers(5,30,25,10,15,1,9,5);
+    console.log(kirat);
--- src/typesScript/app.ts
+++ src/typesScript/app.ts
@@ -1,26 +1,25 @@
 export function probabilityOfPlayers(
-    dot: number,
-    one: number,
-    two: number,
-    three: number,
-    four: number,
-    five: number,
-    six: number,
-    out: number
+  dot: number,
+  one: number,
+  two: number,
+  three: number,
+  four: number,
+  five: number,
+  six: number,
+  out: number
 ): number[] {
-    let arrayOfProbabilities: number[] = [];
+  let arrayOfProbabilities: number[] = [];
 
-    for (let ball: number = 0; ball < dot; ball++) arrayOfProbabilities.push(0);
-    for (let ball: number = 0; ball < one; ball++) arrayOfProbabilities.push(1);
-    for (let ball: number = 0; ball < two; ball++) arrayOfProbabilities.push(2);
-    for (let ball: number = 0; ball < three; ball++) arrayOfProbabilities.push(3);
-    for (let ball: number = 0; ball < four; ball++) arrayOfProbabilities.push(4);
-    for (let ball: number = 0; ball < five; ball++) arrayOfProbabilities.push(5);
-    for (let ball: number = 0; ball < six; ball++) arrayOfProbabilities.push(6);
-    for (let ball: number = 0; ball < out; ball++) arrayOfProbabilities.push(7);
-    return arrayOfProbabilities;
+  for (let ball: number = 0; ball < dot; ball++) arrayOfProbabilities.push(0);
+  for (let ball: number = 0; ball < one; ball++) arrayOfProbabilities.push(1);
+  for (let ball: number = 0; ball < two; ball++) arrayOfProbabilities.push(2);
+  for (let ball: number = 0; ball < three; ball++) arrayOfProbabilities.push(3);
+  for (let ball: number = 0; ball < four; ball++) arrayOfProbabilities.push(4);
+  for (let ball: number = 0; ball < five; ball++) arrayOfProbabilities.push(5);
+  for (let ball: number = 0; ball < six; ball++) arrayOfProbabilities.push(6);
+  for (let ball: number = 0; ball < out; ball++) arrayOfProbabilities.push(7);
+  return arrayOfProbabilities;
 }
-
 
 /* the above function takes the probabilities of the player in an order
 and return an array of length 100, which contains the score specified 
@@ -29,8 +28,10 @@ according to the given probability
 for example: if we give the probabilities of a player as 
 const kirat=probabilityOfPlayers(5,30,25,10,15,1,9,5);
 then it return an array as
-[0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2,
-      2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3,
-       3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-        5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7]   */
+[0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+ 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4,
+ 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7] 
+    according to the definition of probaility in the above array
+    there is 5% of probability to get "0" ,if we pick at random  
+    there is 30% of probability to get "1", if we pick at random and so on..*/
