b99a668483206ba574b7ecea8d30acec9fe61872
raaj <rajesh.y@everest.engineering>
2021-10-05 12:17:49 +0530

    created a test to check at a specified index wether the index has the exact score or not according to the probability mentioned

--- src/tests/app.test.ts
+++ src/tests/app.test.ts
@@ -5,7 +5,11 @@ test("gives the probability of platers as mentioned", () => {
    const lengh=kirat.length;
     expect(lengh).toBe(100);
   });
-  
+  test("failing at a specific index in the array of probability", () => {
+    const kirat=probabilityOfPlayers(5,30,25,10,15,1,9,5);
+    const score=kirat[5]
+     expect(score).toBe(0);
+   });
 
 
 
