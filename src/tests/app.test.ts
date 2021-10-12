b69f80e0fa34e2854fb47af44b876841f9ce0d95
raaj <rajesh.y@everest.engineering>
2021-10-12 11:38:21 +0530

    created the batsmans array with the players

--- src/tests/app.test.ts
+++ src/tests/app.test.ts
@@ -1,58 +1,49 @@
 import { batman } from "../typesScript/app";
 
 test("gives the probability of platers as mentioned", () => {
-    var kirat = new batman("kirat",[5,30,25,10,15,1,9,5]);
-   const lengh=(kirat.returnsArray(kirat.prob)).length;
-    expect(lengh).toBe(100);
-  });
-  test("failing at a specific index in the array of probability", () => {
-    var kirat = new batman("kirat",[5,30,25,10,15,1,9,5]);
-   const score=(kirat.returnsArray(kirat.prob)[4]);
-    expect(score).toBe(0);
-   });
-   test("failing at a specific index in the array of probability", () => {
-    var kirat = new batman("kirat",[5,30,25,10,15,1,9,5]);
-    const score=(kirat.returnsArray(kirat.prob)[5]);
-     expect(score).toBe(1);
-   });
-   test("the playing condition of the batsmen",()=>{
-    const runs: number = Math.floor(Math.random() * 100);
-    var kirat = new batman("kirat",[5,30,25,10,15,1,9,5]);
-    const score= (kirat.returnsArray(kirat.prob)[runs])
-    if(score==7){
+  var kirat = new batman("kirat", [5, 30, 25, 10, 15, 1, 9, 5]);
+  const lengh = kirat.returnsArray(kirat.prob).length;
+  expect(lengh).toBe(100);
+});
+test("failing at a specific index in the array of probability", () => {
+  var kirat = new batman("kirat", [5, 30, 25, 10, 15, 1, 9, 5]);
+  const score = kirat.returnsArray(kirat.prob)[4];
+  expect(score).toBe(0);
+});
+test("failing at a specific index in the array of probability", () => {
+  var kirat = new batman("kirat", [5, 30, 25, 10, 15, 1, 9, 5]);
+  const score = kirat.returnsArray(kirat.prob)[5];
+  expect(score).toBe(1);
+});
+test("the playing condition of the batsmen", () => {
+  const runs: number = Math.floor(Math.random() * 100);
+  var kirat = new batman("kirat", [5, 30, 25, 10, 15, 1, 9, 5]);
+  const score = kirat.returnsArray(kirat.prob)[runs];
+  if (score == 7) {
     expect(kirat.batmanIsOut).toBe(true);
-    }
-    if(score<7){
-        expect(kirat.batmanIsOut).toBe(false);
-        }
-   });
-   test("counting the number of balls played by the player", () => {
-    var kirat = new batman("kirat",[5,30,25,10,15,1,9,5]);
-     kirat.run();
-     expect(kirat.NumberOfBallsPlayed).toBe(1);
-   });
-   test("counting the number of balls played by the player", () => {
-    var kirat = new batman("kirat",[5,30,25,10,15,1,9,5]);
-     kirat.run();
-     kirat.run();
-     kirat.run();
-     expect(kirat.NumberOfBallsPlayed).toBe(3);
-   });
-   test("count the individual score of the player", () => {
-    var kirat = new batman("kirat",[5,30,25,10,15,1,9,5]);
-     const score1 = kirat.run();
-     const score2 = kirat.run();
-     if(score1<7||score2<7)
-     expect(kirat.ScoredRuns).toBe(score1+score2);
-
-   });
-
-
-
-  
-
-  
-
-
+  }
+  if (score < 7) {
+    expect(kirat.batmanIsOut).toBe(false);
+  }
+});
+test("counting the number of balls played by the player", () => {
+  var kirat = new batman("kirat", [5, 30, 25, 10, 15, 1, 9, 5]);
+  kirat.run();
+  expect(kirat.NumberOfBallsPlayed).toBe(1);
+});
+test("counting the number of balls played by the player", () => {
+  var kirat = new batman("kirat", [5, 30, 25, 10, 15, 1, 9, 5]);
+  kirat.run();
+  kirat.run();
+  kirat.run();
+  expect(kirat.NumberOfBallsPlayed).toBe(3);
+});
+test("count the individual score of the player", () => {
+  var kirat = new batman("kirat", [5, 30, 25, 10, 15, 1, 9, 5]);
+  const score1 = kirat.run();
+  const score2 = kirat.run();
+  if (score1 < 7 || score2 < 7) expect(kirat.ScoredRuns).toBe(score1 + score2);
+  expect(kirat.ScoredRuns).toBe(score1+score2);
+});
 
 
--- src/typesScript/app.ts
+++ src/typesScript/app.ts
@@ -1,49 +1,52 @@
-
 export class batman {
-  name:string ;
-  prob:number[];
-  constructor(name:string,prob:number[]){
+  name: string;
+  prob: number[];
+  constructor(name: string, prob: number[]) {
     this.name = name;
     this.prob = prob;
   }
-  public batmanIsOut=false;
-  public ScoredRuns=0
-  public NumberOfBallsPlayed=0
-  
+  public batmanIsOut = false;
+  public ScoredRuns = 0;
+  public NumberOfBallsPlayed = 0;
 
-  returnsArray(prob:number[]){
-  const arrayOfProbability:number[] = [];
-  for (let ball: number = 0; ball < prob[0]; ball++) arrayOfProbability.push(0);
-  for (let ball: number = 0; ball < prob[1]; ball++) arrayOfProbability.push(1);
-  for (let ball: number = 0; ball < prob[2]; ball++) arrayOfProbability.push(2);
-  for (let ball: number = 0; ball < prob[3]; ball++) arrayOfProbability.push(3);
-  for (let ball: number = 0; ball < prob[4]; ball++) arrayOfProbability.push(4);
-  for (let ball: number = 0; ball < prob[5]; ball++) arrayOfProbability.push(5);
-  for (let ball: number = 0; ball < prob[6]; ball++) arrayOfProbability.push(6);
-  for (let ball: number = 0; ball < prob[7]; ball++) arrayOfProbability.push(7);
-  return arrayOfProbability;
-}
-
-run() {
-  const runs: number = Math.floor(Math.random() * 100);
-  const score= (this.returnsArray(this.prob)[runs])
-  this.NumberOfBallsPlayed +=1;
-  if(score==7){
-    this.batmanIsOut=true;
+  returnsArray(prob: number[]) {
+    const arrayOfProbability: number[] = [];
+    for (let ball: number = 0; ball < prob[0]; ball++)
+      arrayOfProbability.push(0);
+    for (let ball: number = 0; ball < prob[1]; ball++)
+      arrayOfProbability.push(1);
+    for (let ball: number = 0; ball < prob[2]; ball++)
+      arrayOfProbability.push(2);
+    for (let ball: number = 0; ball < prob[3]; ball++)
+      arrayOfProbability.push(3);
+    for (let ball: number = 0; ball < prob[4]; ball++)
+      arrayOfProbability.push(4);
+    for (let ball: number = 0; ball < prob[5]; ball++)
+      arrayOfProbability.push(5);
+    for (let ball: number = 0; ball < prob[6]; ball++)
+      arrayOfProbability.push(6);
+    for (let ball: number = 0; ball < prob[7]; ball++)
+      arrayOfProbability.push(7);
+    return arrayOfProbability;
   }
-  if(score<7){
-    this.ScoredRuns += score;
+
+  run() {
+    const runs: number = Math.floor(Math.random() * 100);
+    const score = this.returnsArray(this.prob)[runs];
+    this.NumberOfBallsPlayed += 1;
+    if (score == 7) {
+      this.batmanIsOut = true;
+    }
+    if (score < 7) {
+      this.ScoredRuns += score;
+    }
+    return score;
   }
-  return score;
-
 }
 
+var kirat = new batman("kirat", [5, 30, 25, 10, 15, 1, 9, 5]);
+var dhoni= new batman("dhoni",[10, 40, 20, 5, 10, 1, 4, 10]);
+var rumra= new batman("rumra",[20, 30, 25, 5, 5, 1, 4, 20]);
+var sashi= new batman ("sashi" ,[30, 25, 5, 0, 5, 1, 4, 30]);
 
-
-
-}
-
-
-var kirat = new batman("kirat",[5,30,25,10,15,1,9,5]);
-var dhoni = new batman("dhoni",[10,25,20,15,10,1,10]);
-   
+let batsmanArray=[kirat,dhoni,rumra,sashi];
